fb648f65fd0bb45653d8ae8c10b2d5fec6045132
Nancy <monalnancy@gmail.com>
2015-10-22 22:09:41 +0000

    git being completed

---
+++ app/models/concerns/has_gravatar.rb
@@ -0,0 +1,8 @@
+module HasGravatar
+
+
+def gravatar
+        "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email.downcase)}"
+    end
+    
+end
--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module ABC
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.web_console.whitelisted_ips = '99.226.173.194'
   end
 end
